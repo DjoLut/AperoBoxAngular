6e7ef0cf8f3d89e009e78def328dc4c9c7fb818c
Lutgen <etu32766@henallux.be>
2019-11-22 10:15:55 +0100

    First Push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MiseAJourComponent } from './mise-a-jour/mise-a-jour.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { UtilisateursComponent } from './utilisateurs/utilisateurs.component';
+import { CommentaireComponent } from './commentaire/commentaire.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MiseAJourComponent,
+    AccueilComponent,
+    UtilisateursComponent,
+    CommentaireComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/mise-a-jour/mise-a-jour.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mise-a-jour',
+  templateUrl: './mise-a-jour.component.html',
+  styleUrls: ['./mise-a-jour.component.css']
+})
+export class MiseAJourComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
