113bef7185153596a91a6ac9b15a0dbf58c658fd
Lutgen <etu32766@henallux.be>
2019-11-24 18:59:29 +0100

    Accueil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UtilisateursComponent } from './utilisateurs/utilisateurs.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { CommentaireComponent } from './commentaire/commentaire.component';
+import { MiseAJourComponent } from './mise-a-jour/mise-a-jour.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'accueil', pathMatch: 'full'},
+  {path: 'accueil', component: AccueilComponent},
+  {path: 'utilisateur', component: UtilisateursComponent},
+  {path: 'commentaire', component: CommentaireComponent},
+  {path: 'update', component: MiseAJourComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { MiseAJourComponent } from './mise-a-jour/mise-a-jour.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { UtilisateursComponent } from './utilisateurs/utilisateurs.component';
 import { CommentaireComponent } from './commentaire/commentaire.component';
+import { CommonModule } from '@angular/common';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { CommentaireComponent } from './commentaire/commentaire.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
