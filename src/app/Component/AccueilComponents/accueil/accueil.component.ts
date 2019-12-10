57999fd1e6dc40c61681ceb406110ef2ea98d1c3
Lutgen <etu32766@henallux.be>
2019-12-10 20:39:42 +0100

    Authentication + Roles

--- src/app/Component/AccueilComponents/accueil/accueil.component.ts
+++ src/app/Component/AccueilComponents/accueil/accueil.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from 'src/app/Service/authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-accueil',
@@ -7,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccueilComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    UserName: new FormControl(null, Validators.required),
+    Password: new FormControl(null, Validators.required)
+  })
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  login() {
+    var username = this.loginForm.get('UserName').value;
+    var password = this.loginForm.get('Password').value;
+    this.authenticationService.login(username, password).subscribe(frm => {
+      this.authenticationService.setToken(frm);
+      this.router.navigate(["/utilisateur"]);
+    });
   }
 
 }
---
+++ src/app/Service/authentication.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { JwtToken } from '../Model/JwtToken';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Constantes } from '../Constantes';
+import { Observable } from 'rxjs';
+import { Utilisateur } from '../Model/Utilisateur';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private _accessToken: JwtToken;
+  private httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
+  constructor(private http: HttpClient) { }
+
+  public setToken(token: JwtToken) {
+    this._accessToken = token;
+  }
+
+  public getToken(): JwtToken {
+    return this._accessToken;
+  }
+
+  public isAuthenticated(): boolean {
+    return this._accessToken !== null && this._accessToken!==undefined;
+  }
+
+  public login(username: string, password: string) {
+    let body = "{\n" +
+      "\t\"Username\":\""+username+"\",\n" +
+      "\t\"Password\":\""+password+"\"\n" +
+    "}";
+    return this.http.post<JwtToken>(`${Constantes.URL_API}Jwt`, body, this.httpOptions);
+  }
+
+}
