790bac10407d1876e387ddc5c08fcf210c936eff
Lutgen <etu32766@henallux.be>
2020-01-13 16:44:15 +0100

    Orthographe

--- Source/src/app/Constantes.ts
+++ Source/src/app/Constantes.ts
@@ -3,7 +3,7 @@ export class Constantes
     public static URL_API = "https://aperoboxapi.azurewebsites.net/api/";
     public static URL_API_LOCALHOST = "https://localhost:5001/api/";
     public static ERREUR400 = "Erreur de contenu (formulaire) ! ";
-    public static ERREUR401 = "Accès non autorisée ! Session expirée ! ";
+    public static ERREUR401 = "Accès non autorisé ! Session expirée ! ";
     public static ERREUR403 = "Vous n'avez pas les droits suffisants ! ";
     public static ERREUR409 = "Conflit. Accès concurrents ! ";
     public static ERREUR500 = "Erreur connexion avec l'api ! ";
